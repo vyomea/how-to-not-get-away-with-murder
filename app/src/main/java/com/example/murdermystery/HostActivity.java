286bfb36425e05259c7447168b11a0f1425110b7
ashutoshlamba <55332461+ashutoshlamba@users.noreply.github.com>
2022-01-15 20:18:11 -0700

    added select map activity

--- app/src/main/java/com/example/murdermystery/HostActivity.java
+++ app/src/main/java/com/example/murdermystery/HostActivity.java
@@ -2,7 +2,9 @@ package com.example.murdermystery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 public class HostActivity extends AppCompatActivity {
@@ -14,5 +16,10 @@ public class HostActivity extends AppCompatActivity {
 
         //ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_list_view,R.id.textView,StringArray);
 
+
+    }
+    public void goToSelectActivity (View view){
+        Intent intent = new Intent (this, SelectMap.class);
+        startActivity(intent);
     }
 }
