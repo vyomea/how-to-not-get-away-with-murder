6b13a57d9fe872ddd30edc0cb5b69b9da6464717
Harshit Malik <harshit4731@gmail.com>
2022-01-15 20:34:51 -0700

    Siiiuuuuuuu

--- app/src/main/java/com/example/murdermystery/MainActivity.java
+++ app/src/main/java/com/example/murdermystery/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.murdermystery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+
+    public void goToCreateActivity(View view) {
+        Intent intent = new Intent(this,CreateActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToJoinActivity(View view) {
+        Intent intent = new Intent(this,JoinActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToHostActivity(View view) {
+        Intent intent = new Intent(this,HostActivity.class);
+        startActivity(intent);
+    }
+
+
+    }
