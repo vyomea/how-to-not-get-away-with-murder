62b70bf3ca3976f4bf322c574181d341f1cc1185
ashutoshlamba <55332461+ashutoshlamba@users.noreply.github.com>
2022-01-16 00:00:31 -0700

    added select map functionality

--- app/src/main/java/com/example/murdermystery/HostActivity.java
+++ app/src/main/java/com/example/murdermystery/HostActivity.java
@@ -6,18 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class HostActivity extends AppCompatActivity {
-
+    ListView v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
-
         //ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_list_view,R.id.textView,StringArray);
-
-
+        Bundle extras = getIntent().getExtras();
+        int mapId = 0;
+        if (extras != null) {
+            mapId = extras.getInt("selectedMap");
+            //The key argument here must match that used in the other activity
+        }
+        if(mapId!=0){
+            TextView mapName = this.findViewById(R.id.textView);
+            mapName.setText(String.valueOf(mapId));
+        }
     }
+
     public void goToSelectActivity (View view){
         Intent intent = new Intent (this, SelectMap.class);
         startActivity(intent);
--- app/src/main/java/com/example/murdermystery/SelectMap.java
+++ app/src/main/java/com/example/murdermystery/SelectMap.java
@@ -1,14 +1,68 @@
 package com.example.murdermystery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SelectMap extends AppCompatActivity {
+
+    ListView mapList;
+    ArrayAdapter<Map> mapsAdapter;
+    ArrayList<Map> mapsList;
+    Button selectMapButton;
+    int pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_map);
+        mapList = findViewById(R.id.map_list);
+        mapsList = new ArrayList<>();
+        mapsList.add(new Map(null,"cardi b is trash","bruhh",100));
+        mapsList.add(new Map(null,"billie eilish is trash","hhhhhhh",101));
+        mapsAdapter = new CustomMapList(this, mapsList);
+        mapList.setAdapter(mapsAdapter);
+        mapList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                pos = position;
+
+                Button selectButton = findViewById(R.id.confirm_button);
+                selectButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (pos < mapList.getCount() && pos >= 0) {
+                            Map map = mapsAdapter.getItem(pos);
+                            Intent intent = new Intent (view.getContext(), HostActivity.class);
+                            intent.putExtra("selectedMap",map.getId());
+                            startActivity(intent);
+                        }
+                    }
+                });
+                Button viewButton = findViewById(R.id.viewDescription);
+                viewButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (pos < mapList.getCount() && pos >= 0) {
+                            Map map = mapsAdapter.getItem(pos);
+                            Intent intent = new Intent (view.getContext(), ShowDescription.class);
+                            intent.putExtra("description",map.getDescription());
+                            startActivity(intent);
+
+                        }
+                    }
+                });
+            }
+        });
     }
+
 }
